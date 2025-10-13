9d8673e708680559807e8df622c6cf8cf6b76088
yeroneee133-cyber <yeroneee133@gmail.com>
2025-10-13 10:00:44 +0300

    Add files via upload

---
+++ Adtech.js
@@ -0,0 +1,15 @@
+// Smooth scroll effect
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+console.dir("document");
+  document.body.style.backgroundColor = ("white")
+
+
+
